9ae720c5b5a6e3ab5c3fd09df27008c7c0d2be71
Federico Castelli <fc@oracleofde.fi>
2022-11-11 14:58:18 +0100

    Create useEditProofTitle() hook

---
+++ src/hooks/proof/useEditProofTitle.ts
@@ -0,0 +1,57 @@
+import {useState} from "react";
+import {GenerateProofParams, UseGenerateProofsState} from "./useGenerateProofs";
+import {useAccount, useContractWrite, useNetwork, usePrepareContractWrite} from "wagmi";
+import {CONTRACTS_DETAILS} from "../../utils/constants";
+import {ethers} from "ethers";
+
+export type EditProofTitleParams = {
+  nftId: string,
+  newTitle: string
+}
+
+/**
+ * @param {boolean} completed
+ * @param {string} error
+ * @param {LoadPricesResult} result
+ */
+export interface UseEditProofTitleState {
+  completed: boolean,
+  error: string,
+  transactionHash: string
+}
+
+/**
+ * @param {function} loadPrices
+ */
+export interface UseEditProofTitleResponse extends UseEditProofTitleState {
+  editProofTitle: (params: EditProofTitleParams) => void
+}
+
+export const useEditProofTitle = () => {
+  const [status, setStatus] = useState<UseEditProofTitleState>({
+    completed: false, error: "", transactionHash: ""});
+  const network = useNetwork();
+  const userAccount = useAccount();
+  const { config, error } = usePrepareContractWrite({
+    address: CONTRACTS_DETAILS[network.chain.id].TPROOF_ROUTER_ADDRESS,
+    abi: CONTRACTS_DETAILS[network.chain.id].TPROOF_ROUTER_ABI,
+    functionName: 'updateTitle',
+    onError: (error) => { setStatus({completed: true, transactionHash: "", error: error.message}); },
+    onSuccess: (data) => { setStatus({completed: true, transactionHash: "", error: ""})}
+  });
+  const contractWrite = useContractWrite(config);
+  const editProofTitle = (params: EditProofTitleParams) => {
+    new Promise(async (resolve, reject) => {
+      await contractWrite.writeAsync({
+        recklesslySetUnpreparedArgs: [
+          [params.nftId], [params.newTitle]],
+        recklesslySetUnpreparedOverrides: {
+          from: userAccount.address
+        }
+      });
+    }).then(() => {});
+  };
+  return {
+    ...status, editProofTitle
+  };
+}
