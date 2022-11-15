19d5f903a9553881c8a5bbec8fc23e970535d803
Federico Castelli <fc@oracleofde.fi>
2022-11-15 09:40:16 +0100

    Fix useGenerateProofs() hook logic

--- src/hooks/contracts/tProofRouter/useGenerateProofs.ts
+++ src/hooks/contracts/tProofRouter/useGenerateProofs.ts
@@ -1,16 +1,16 @@
-import {useEffect} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {Prices, ProofToMint} from "../../../utils/ProjectTypes/Project.types";
-import {useAccount, useContractWrite, useNetwork, usePrepareContractWrite} from "wagmi";
+import {useAccount, useContractWrite, useNetwork, usePrepareContractWrite, useWaitForTransaction} from "wagmi";
 import {CONTRACTS_DETAILS} from "../../../utils/constants";
 import {ethers} from "ethers";
 import {useBaseSmartContractWrite, useBaseSmartContractWriteState} from "../../utils/useBaseSmartContractWrite";
 
 /**
- * @param {ProofToMint[]} proofs - proofs to be passed
+ * @param {ProofToMint[]} proofs - proofs to be minted
  * @param {string} delegatorAddress - address of the delegator to verify the hash if file is made public
  * @param {Prices} price - the prices for this network
  */
-export type GenerateProofParams = {
+export type UseGenerateProofParams = {
   proofs: ProofToMint[],
   delegatorAddress: string,
   price: Prices
@@ -20,63 +20,73 @@ export type GenerateProofParams = {
  * @param {function} generateProofs
  */
 export interface UseGenerateProofsResponse extends useBaseSmartContractWriteState<undefined> {
-  generateProofs: (params: GenerateProofParams) => void
+  generateProofs: () => void
 }
 
 /**
  * Generates the proofs
  */
-export const useGenerateProofs = (): UseGenerateProofsResponse => {
+export const useGenerateProofs = (params: UseGenerateProofParams): UseGenerateProofsResponse => {
   const {completed, error, loading, result, txHash, endAsyncActionError, endAsyncActionSuccess, startAsyncAction,
     startAsyncActionWithTxHash} = useBaseSmartContractWrite<undefined>();
   const network = useNetwork();
   const userAccount = useAccount();
+  const [doCall, setDoCall] = useState<boolean>(false);
+  const args = useMemo(() => {
+
+    let hash: string[] = [];
+    let title: string[] = [];
+    let withFileUrl: boolean[] = [];
+    let storageType: number[] = [];
+
+    // eval the params
+    for (let p of params.proofs) {
+      hash.push("0x" + p.hash);
+      title.push(p.title);
+      withFileUrl.push(p.toBeVerified);
+      storageType.push(0);  // currently we support only ArweaveV1
+    }
+    return [
+      hash, title, withFileUrl, storageType, userAccount.address, params.delegatorAddress
+    ]
+  }, [params]);
+  let totalAmountEth = (
+          params.proofs.map((proof): number => proof.toBeVerified ? 1 : 0)
+              .reduce((a, b) => a + b)
+      ) * params.price.verification
+      + (params.proofs.length * params.price.mint);
   const prepareContractWrite = usePrepareContractWrite({
     address: CONTRACTS_DETAILS[network.chain?.id]?.TPROOF_ROUTER_ADDRESS,
     abi: CONTRACTS_DETAILS[network.chain?.id]?.TPROOF_ROUTER_ABI,
     functionName: 'createProofs',
-    onError: (error) => { endAsyncActionError(error.message); },
-    onSuccess: (data) => { endAsyncActionSuccess(undefined); },
-    enabled: false
+    enabled: doCall,
+    args: args,
+    overrides: {
+      value: ethers.utils.parseEther(totalAmountEth.toString()),
+    }
   });
   const contractWrite = useContractWrite(prepareContractWrite.config);
+  const waitForTx = useWaitForTransaction({
+    hash: contractWrite.data?.hash,
+  });
 
   useEffect(() => {
-    if (contractWrite.data?.hash)
-      startAsyncActionWithTxHash(contractWrite.data?.hash);
-  }, [contractWrite.data?.hash]);
+    if (waitForTx.status === "success") endAsyncActionSuccess(undefined)
+    else if (waitForTx.status === "loading") startAsyncActionWithTxHash(contractWrite.data?.hash)
+    else if (waitForTx.status === "error") endAsyncActionError(waitForTx.error.message)
+  }, [waitForTx.status]);
 
-  const generateProofs = (params: GenerateProofParams): void => {
+  useEffect(() => {
+    if (doCall) {
+      contractWrite.write();
+      setDoCall(false);
+    }
+  }, [doCall]);
+
+  const generateProofs = (): void => {
     startAsyncAction();
-    new Promise( async (resolve, reject) => {
-      let totalAmountEth = (
-              params.proofs.map((proof): number => proof.toBeVerified ? 1 : 0)
-                  .reduce((a, b) => a + b)
-          ) * params.price.verification
-          + (params.proofs.length * params.price.mint);
-
-      let hash: string[] = [];
-      let title: string[] = [];
-      let withFileUrl: boolean[] = [];
-      let storageType: number[] = [];
-
-      //eval the params
-      for (let p of params.proofs) {
-        hash.push("0x" + p.hash);
-        title.push(p.title);
-        withFileUrl.push(p.toBeVerified);
-        storageType.push(0);  // currently we support only ArweaveV1
-      }
-      await contractWrite.writeAsync({
-        recklesslySetUnpreparedArgs: [
-            hash, title, withFileUrl, storageType, userAccount.address, params.delegatorAddress],
-        recklesslySetUnpreparedOverrides: {
-          value: ethers.utils.parseEther(totalAmountEth.toString()),
-          // gas: Math.floor(parseInt(gas) * 1.1),
-          // maxPriorityFeePerGas: params.web3.utils.toWei("0.000000080", 'ether')  // TODO use a Gas Station, checking this issue https://github.com/ethers-io/ethers.js/issues/2828
-        }
-      });
-    }).then(() => {});
+    setDoCall(true);
   };
+
   return { completed, error, loading, result, txHash, generateProofs};
 }
